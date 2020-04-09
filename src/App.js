2a184e9e696fa1b446e0ad41b812aa6b0cf6eb63
Huzzky <vladislav.bychkov01@gmail.com>
2020-04-09 17:55:58 +0300

    master

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import NAV from './components/nav';
 
 function App() {
   return (
-    <div>
-      
+    <div className="all">
+      <NAV/>
     </div>
   );
 }
--- src/components/nav.js
+++ src/components/nav.js
@@ -1,20 +1,15 @@
 import React from 'react';
+import './css/nav.css';
+// import iconInfinity from './icon/main-icon.svg';
 
 function NAV() {
     return(
         <div className="nav-div">
             <nav>
-                <ul className="nav-ul">
-                    <li className="nav-txt"><img src="" alt=""/></li> {/*Здесь должна быть ссылка*/}
-                    <li className="nav-txt">Создание сайтов</li>
-                    <li className="nav-txt">Продвижение сайтов</li>
-                    <li className="nav-txt">Аналитика сайтов</li>
-                    <li className="nav-txt">Цены</li>
-                    <li className="nav-txt">Контакты</li>
-                    <li className="nav-txt"><button>Заказать услугу</button></li>
-                </ul>
-        
+                
             </nav>
         </div>
     );
 } 
+
+export default NAV;
