48d599c324bae4de44d9bde8d242df639f1c8e2d
Huzzky <vladislav.bychkov01@gmail.com>
2020-04-09 21:39:03 +0300

    master

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,15 +1,36 @@
 import React from 'react';
 import './css/nav.css';
-// import iconInfinity from './icon/main-icon.svg';
+import videohead from './videos/video.mp4';
 
-function NAV() {
+class NAV extends React.Component {
+    constructor (props) {
+        super(props);
+
+        this.state = {
+            
+        }
+    }
+
+render(){
     return(
         <div className="nav-div">
+            <div className="div-vid">
+                <video autoPlay loop muted className="video-header">
+                    <source src={videohead}></source>
+                </video>
+            </div>
             <nav>
-                
+                <ul>
+                    <li className="nav-txt">Scenic</li>
+                    <li className="nav-txt">HOME</li>
+                    <li className="nav-txt">STUDIO</li>
+                    <li className="nav-txt">OUR PEOPLE</li>
+                    <li className="nav-txt">LET'S TALK</li>
+                </ul>
             </nav>
+
         </div>
     );
-} 
+}}
 
 export default NAV;
