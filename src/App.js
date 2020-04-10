a1a74fd780d4a4ccdba591081e6c7355b8a36630
Huzzky <vladislav.bychkov01@gmail.com>
2020-04-11 00:32:22 +0300

    master

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import NAV from './components/nav';
+import About from './components/about';
 
 function App() {
   return (
     <div className="all">
       <NAV/>
+      <About/>
     </div>
   );
 }
---
+++ src/components/about.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class About extends React.Component {
+    render () {
+        return (
+            <div>
+                <div>
+                    <h5>INTRODUCING SCENIC</h5>
+                    <h2>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...</h2>
+                </div>
+                <div>
+                    <img src="" alt="" className=""/>
+                    <img src="" alt="" className=""/>
+                    <img src="" alt="" className=""/>
+                    <img src="" alt="" className=""/>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default About;
