4fe661bc2510328750747206cbed8c6ebf127c2a
Huzzky <vladislav.bychkov01@gmail.com>
2020-04-11 00:06:11 +0300

    master

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,50 +1,23 @@
 import React from 'react';
 import './css/nav.css';
-import videohead from './videos/video.mp4';
+import videohead from './videos/video.webm';
 
 class NAV extends React.Component {
     constructor (props) {
         super(props);
         this.state = {
             classNameDiv:'dop-div',
-            scroolmy:0
 
         }}
     scrollSite(y){ //Функция для считывания Скрулла
-        // this.setState(prevState => {
-        //     return {
-        //         scroolmy: prevState+=100
-        //     }
-        // })
-        // if (y<0 && this.state.scroolmy<12){
-        //     this.state.scroolmy++;
-        //     this.setState({classNameDiv:"dop-div"});
-        // }
-        // else if (y>0){
-        //     if (this.state.scroolmy===0){
-        //         this.setState({classNameDiv:"dop-div"});
-        //     }
-        //     else{this.state.scroolmy--;}
-        // }
-        // console.log(this.state.scroolmy);
 
         if (window.scrollY<100) {
             this.setState({classNameDiv:'dop-div'});
             console.log(1, window.scrollY);
-        } else if (window.scrollY > 100) {
+        } else {
             this.setState({classNameDiv:'dop-div-2'});
             console.log(2, window.scrollY);
         }
-
-
-        // if (y>100) {
-        //     this.setState({classNameDiv:'dop-div-2'});
-        //     console.log(this.state.scroolmy);
-        // }
-        // else if (y<100) {
-        //     this.setState({classNameDiv:"dop-div"});
-        //     console.log(this.scroolmy);
-        // }
     }
 
     componentWillMount(){
