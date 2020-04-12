607f0f3e0533b86f9d380b813135fa9f4514b273
Huzzky <vladislav.bychkov01@gmail.com>
2020-04-13 02:24:54 +0300

    master

--- src/components/about.js
+++ src/components/about.js
@@ -14,35 +14,44 @@ class About extends React.Component {
                     <h1 className="txt-h1-abt">This template is designed for you. Sed ornare, tortor nec placerat lacinia, leo quam rutrum leo,
                     eget posuere ipsum sem eu justo. Integer nunc libero</h1>
                 </div>
+
                 <div className='img-div cs-style-1'>
+                    <div className="fr">
                     <a href="fi"><figure>
-                        <img src={fimageabout} alt="Первая картинка" className="img-about fr"/>
+                        <img src={fimageabout} alt="Первая картинка" className="img-about"/>
                         <figcaption className="fig-1">
                             <div className="fon-black"></div>
-                            <h3 className="about-img">The Best Books</h3>
+                            <h3 className="first-h3">The Best Books</h3>
                         </figcaption>                 
-                    </figure></a>
+                    </figure></a></div>
+
+                    <div className="fr">
                     <a href="se"><figure>
-                        <img src={simageabout} alt="Вторая картинка" className="img-about fr"/>
+                        <img src={simageabout} alt="Вторая картинка" className="img-about"/>
                         <figcaption className="fig-1">
                         <div className="fon-black"></div>
-                        <h3 className="about-img">The Best Books</h3>
+                        <h3 className="first-h3">The Best Books</h3>
                         </figcaption>
-                    </figure></a>
+                    </figure></a></div>
+
+                    <div className="sr">
                     <a href="th"> <figure>
-                        <img src={timageabout} alt="Третья картинка" className="img-about sr"/>
+                        <img src={timageabout} alt="Третья картинка" className="img-about "/>
                         <figcaption className="fig-2">
                         <div className="fon-black-2"></div>
-                        <h3 className="about-img">The Best Books</h3>
+                        <h3 className="second-h3">The Best Books</h3>
                         </figcaption>
-                    </figure></a>
+                    </figure></a></div>
+
+                    <div className="sr">
                     <a href="fo"> <figure>
-                        <img src={foimageabout} alt="Четвертая картинка" className="img-about sr"/>
+                        <img src={foimageabout} alt="Четвертая картинка" className="img-about "/>
                         <figcaption className="fig-2">
                         <div className="fon-black-2"></div>
-                        <h3 className="about-img">The Best Books</h3>
+                        <h3 className="second-h3">The Best Books</h3>
                         </figcaption>
-                    </figure></a>
+                    </figure></a></div>
+
                 </div>
             </div>
         );
