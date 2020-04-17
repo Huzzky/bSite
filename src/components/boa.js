e8f9da46c63a1e2bbbe1faf9436b6386ec407e28
Huzzky <vladislav.bychkov01@gmail.com>
2020-04-17 22:18:13 +0300

    master

--- src/components/boa.js
+++ src/components/boa.js
@@ -5,7 +5,75 @@ class BOA extends React.Component {
     render () {
         return (
             <div className="boa-all">
-                <h1 className="h1-boa">Best Our Authors</h1>
+                <div>
+                <h1 className="h1-boa">Best Our Authors</h1></div>
+                <div className="img-authors-div">
+                    <ul className="ul-img-auth">
+                        <li className="img-li-auth">
+                            <figure>
+                                <img src="" alt="" className="img-auth"/>
+                                    <figcaption>
+                                        <li><img src="" alt="" className="icon-auth-sw"/></li>
+                                        <li><img src="" alt="" className="icon-auth-sw"/></li>
+                                    </figcaption>
+                            </figure>
+                        </li>
+                        <li className="img-li-auth">
+                            <figure>
+                                <img src="" alt="" className="img-auth"/>
+                                    <figcaption>
+                                        <li><img src="" alt="" className="icon-auth-sw"/></li>
+                                        <li><img src="" alt="" className="icon-auth-sw"/></li>
+                                    </figcaption>
+                            </figure>
+                        </li>
+                        <li className="img-li-auth">
+                            <figure>
+                                <img src="" alt="" className="img-auth"/>
+                                    <figcaption>
+                                        <li><img src="" alt="" className="icon-auth-sw"/></li>
+                                        <li><img src="" alt="" className="icon-auth-sw"/></li>
+                                    </figcaption>
+                            </figure>
+                        </li>
+                        <li className="img-li-auth">
+                            <figure>
+                                <img src="" alt="" className="img-auth"/>
+                                    <figcaption>
+                                        <li><img src="" alt="" className="icon-auth-sw"/></li>
+                                        <li><img src="" alt="" className="icon-auth-sw"/></li>
+                                    </figcaption>
+                            </figure>
+                        </li>
+                        <li className="img-li-auth">
+                            <figure>
+                                <img src="" alt="" className="img-auth"/>
+                                    <figcaption>
+                                        <li><img src="" alt="" className="icon-auth-sw"/></li>
+                                        <li><img src="" alt="" className="icon-auth-sw"/></li>
+                                    </figcaption>
+                            </figure>
+                        </li>
+                        <li className="img-li-auth">
+                            <figure>
+                                <img src="" alt="" className="img-auth"/>
+                                    <figcaption>
+                                        <li><img src="" alt="" className="icon-auth-sw"/></li>
+                                        <li><img src="" alt="" className="icon-auth-sw"/></li>
+                                    </figcaption>
+                            </figure>
+                        </li>
+                        <li className="img-li-auth">
+                            <figure>
+                                <img src="" alt="" className="img-auth"/>
+                                    <figcaption>
+                                        <li><img src="" alt="" className="icon-auth-sw"/></li>
+                                        <li><img src="" alt="" className="icon-auth-sw"/></li>
+                                    </figcaption>
+                            </figure>
+                        </li>
+                    </ul>
+                </div>
             </div>
         );
     }
