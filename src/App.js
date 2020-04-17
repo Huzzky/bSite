462df8be4a10d25ef73ca866ac6120b9de4d5709
Huzzky <vladislav.bychkov01@gmail.com>
2020-04-17 20:58:15 +0300

    master

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import NAV from './components/nav';
 import About from './components/about';
+import BOA from './components/boa';
 
 function App() {
   return (
     <div className="all">
       <NAV/>
       <About/>
+      <BOA/>
     </div>
   );
 }
---
+++ src/components/boa.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './css/boa.css';
+
+class BOA extends React.Component {
+    render () {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default BOA;
