0b1e8287261fbafde8b4fdac6390843f516daef5
Huzzky <vladislav.bychkov01@gmail.com>
2020-04-19 23:52:29 +0300

    master

--- src/components/boa.js
+++ src/components/boa.js
@@ -7,27 +7,37 @@ import auth4 from './images/auth-4.jpg';
 import auth5 from './images/auth-5.jpg';
 
 class BOA extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            auth: auth1,
+            count: 1,
+            countimg: 0,
+        };
+        this.clickImg = this.clickImg.bind(this);
+    }
+    clickImg() {
+        this.setState(prevState => {
+            return{
+                count: prevState+1,
+            }})
+    }
     render () {
         return (
             <div className="boa-all">
                 <div className="div-all-2">
-                    <h1 className="h1-boa">Best Our Authors</h1>               
-                <div className="img-authors-div">
-                    <div className="div-img">                
-                            <img src={auth1} alt="" className="img-auth"/>                   
-                    </div>
-                    <div className="div-img">                       
-                            <img src={auth2} alt="" className="img-auth"/>                           
-                    </div>
-                    <div className="div-img">                     
-                            <img src={auth3} alt="" className="img-auth"/>                        
-                    </div>
-                    <div className="div-img">
-                        <img src={auth4} alt="" className="img-auth"/>                                          
-                    </div>
-                    <div className="div-img">
-                        <img src={auth5} alt="" className="img-auth"/>                                       
-                    </div>
+                    <h1 className="h1-boa">Best Our Authors</h1>   
+
+
+                    <div className='img-div cs-style-2'>
+                    <figure className="fig-img">
+                        <div className="img-trans-auth">             
+                            <img src={this.state.auth} alt="" className="img-auth"/>  
+                            <figcaption className="fig-img-1">
+                            <button className="mark marker-1">></button><button className="mark marker-2">></button>
+                        </figcaption></div>
+                        </figure>
+
                 </div>
                 </div>
             </div>
