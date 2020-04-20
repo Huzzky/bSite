8a37e0e78959c6e28d64840bf62e8f1dfbd3b80b
Huzzky <vladislav.bychkov01@gmail.com>
2020-04-20 11:04:24 +0300

    master

--- src/components/boa.js
+++ src/components/boa.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import "react-image-gallery/styles/css/image-gallery.css";
+// node_modules/react-image-gallery/styles/scss/image-gallery-no-icon.scss
+// node_modules/react-image-gallery/styles/css/image-gallery-no-icon.css
+import ImageGallery from 'react-image-gallery';
 import './css/boa.css';
 import auth1 from './images/auth-1.jpg';
 import auth2 from './images/auth-2.jpg';
 import auth3 from './images/auth-3.jpg';
 import auth4 from './images/auth-4.jpg';
 import auth5 from './images/auth-5.jpg';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 class BOA extends React.Component {
     constructor(props) {
@@ -52,12 +73,24 @@ class BOA extends React.Component {
         }
     }
     render () {
+        const images = [
+            {
+              original: auth1
+            },
+            {
+              original: auth2
+            },
+            {
+              original: auth3
+              
+            }
+          ]
         return (
             <div className="boa-all">
                 <div className="div-all-2">
                     <h1 className="h1-boa">Sic parvis magna</h1>
                     <div className="img-and-abt">   
-                        <div className='img-div cs-style-2'>
+                        {/* <div className='img-div cs-style-2'>
                             <figure className="fig-img">
                                 <div className="img-trans-auth">             
                                     <img src={this.state.auth} alt="" className="img-auth"/>  
@@ -66,10 +99,15 @@ class BOA extends React.Component {
                                     </button><button className="mark marker-2" onClick={this.clickImgForward}>></button>
                                 </figcaption></div>
                                 </figure>           
-                        </div>
+                        </div> */}
+                        <ImageGallery items={images}/>
                         <div className="about-auth-div">
                             <h1 className="abt-auth-h1">Lorem Ipsum</h1>
                             <p className="abt-auth-p">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris cursus commodo gravida. 
+                            In nisi nibh, iaculis id vulputate eu, lacinia eu urna. Nam mattis nec risus a vulputate. Class aptent.
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris cursus commodo gravida. 
+                            In nisi nibh, iaculis id vulputate eu, lacinia eu urna. Nam mattis nec risus a vulputate. Class aptent.
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris cursus commodo gravida. 
                             In nisi nibh, iaculis id vulputate eu, lacinia eu urna. Nam mattis nec risus a vulputate. Class aptent.</p>
                         </div>
                     </div>
