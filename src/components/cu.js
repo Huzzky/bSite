7e43d1ee7bd436287e0ab4ca4765cc734e684a75
Huzzky <vladislav.bychkov01@gmail.com>
2020-04-25 21:08:51 +0300

    master

--- src/components/cu.js
+++ src/components/cu.js
@@ -7,12 +7,14 @@ class ContactUS extends React.Component {
             <div className="all-cu">
                 <div className="fon-cu">
                     <form action="post" className="fm-cu">
+                        <div className="form-div">
                         <div className="n-e">
                             <input type="text" className="i-txt"/>
                             <input type="text" className="i-txt"/>
                         </div>
                         <div><input type="text"/></div>
                         <div><input type="text"/></div>
+                        </div>
                     </form>
                 </div>
             </div>
