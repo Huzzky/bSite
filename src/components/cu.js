2ec8c3a920f58a7a752f2e98a37fc718c41bd2c7
Huzzky <vladislav.bychkov01@gmail.com>
2020-05-03 22:30:40 +0300

    master

--- src/components/cu.js
+++ src/components/cu.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import './css/cu.css';
+import fonCU from './images/fon-contact.jpg';
 
 class ContactUS extends React.Component {
     render() {
         return (
-            <div className="all-cu">
+            <div className="all-cu"
+            style={{backgroundImage: 'url(' + fonCU + ')'}}
+            >
                 <div className="fon-cu">
+                    <h1 className="txt-cu">Contact Us</h1>
                     <form action="post" className="fm-cu">
                         <div className="form-div">
-                        <div className="n-e">
-                            <input type="text" className="i-txt"/>
-                            <input type="text" className="i-txt"/>
-                        </div>
-                        <div className='div-input'><input type="text" className="input-txt"/></div>
-                        <div className="div-input"><input type="text" className="input-txt"/></div>
+                            <div className="n-e-div">
+                                <div className="n-e">
+                                    <input type="text" className="i-txt-name"/>
+                                    <input type="text" className="i-txt-phone"/>
+                                </div>
+                            </div>
+                            <div className='div-input'><input type="text" className="input-txt-email"/></div>
+                            <div className="div-input"><input type="text" className="input-txt-message"/></div>
                         </div>
                     </form>
                 </div>
