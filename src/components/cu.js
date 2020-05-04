fa7627a46b32a270a3c64229970d9d1c11e6e96d
Huzzky <vladislav.bychkov01@gmail.com>
2020-05-04 20:38:54 +0300

    master

--- src/components/cu.js
+++ src/components/cu.js
@@ -3,6 +3,28 @@ import './css/cu.css';
 import fonCU from './images/fon-contactus.jpg';
 
 class ContactUS extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            valueName: "Your name...",
+            valuePhone: "Your phone...",
+            valueEmail: "Your email...",
+            valueMessage: "Your message...",
+
+        }
+        this.focusOn = this.focusOn.bind(this);
+        this.focusOff = this.focusOff.bind(this);
+    }
+    focusOn(){
+        if (this.state.valueEmail==="Your email...") {
+            this.state.valueEmail="";
+        }
+    }
+    focusOff(){
+        console.log("8759")
+    }
+
+
     render() {
         return (
             <div className="all-cu" style={{backgroundImage: 'url(' + fonCU + ')'}}>
@@ -14,12 +36,13 @@ class ContactUS extends React.Component {
                         <div className="form-div">
                             <div className="n-e-div">
                                 <div className="n-e">
-                                    <input type="text" className="i-txt-name"/>
-                                    <input type="text" className="i-txt-phone"/>
+                                    <input type="text" placeholder="Your name..." className="i-txt-name"/>
+                                    <input type="tel" placeholder="Your phone..." className="i-txt-phone"/>
                                 </div>
                             </div>
-                            <div className='div-input'><input type="text" className="input-txt-email"/></div>
-                            <div className="div-input"><textarea name="" id="" cols="15" rows="5" className="input-txt-message"></textarea></div>
+                            <div className='div-input'><input type="email" placeholder="Email" className="input-txt-email" onBlur={this.focusOff} onFocus={this.focusOn}/></div>
+                            <div className="div-input"><textarea placeholder="Your message..." name="" id="" cols="15" rows="5" className="input-txt-message"></textarea></div>
+                            <button className="send-form-cu">Send</button>
                         </div>
                     </form>
                 </div>
